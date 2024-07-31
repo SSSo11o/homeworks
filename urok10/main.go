a5eba6a09e29db3ce446c1fa6bd13bcde35a2ea4
SSSo11o <ibronovaziz5@gmail.com>
2024-07-31 20:05:23 +0500

    lesson10

---
+++ urok10/main.go
@@ -0,0 +1,18 @@
+//                          Metodi strukrura
+
+package main
+
+import "fmt"
+
+type Rectangle struct {
+	width, height float64
+}
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+}
+
+func main (){
+	rect := Rectangle{width: 10, height: 5}
+	area := rect.Area()
+	fmt.Println(area)
+}
